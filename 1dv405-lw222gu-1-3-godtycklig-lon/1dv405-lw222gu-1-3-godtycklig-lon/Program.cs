ff9b57afaacef2621c9e718ad805cbc69feaac5f
Lisa Westlund <lw222gu@student.lnu.se>
2014-10-02 10:48:48 +0200

    Slutlig redovisad
    Genomfört beräkningar, och ordnat felhanteringen. Uppställning av
    antalet inmatade löner, samt möjligheten att fortsätta köra programmet
    och göra nya beräkningar.

--- 1dv405-lw222gu-1-3-godtycklig-lon/1dv405-lw222gu-1-3-godtycklig-lon/Program.cs
+++ 1dv405-lw222gu-1-3-godtycklig-lon/1dv405-lw222gu-1-3-godtycklig-lon/Program.cs
@@ -12,53 +12,98 @@ namespace _1dv405_lw222gu_1_3_godtycklig_lon
         {
             int numberOfInputSalaries;
 
-            numberOfInputSalaries = ReadInt("Ange antal löner du vill mata in: ");
-
-            if (numberOfInputSalaries < 2)
+            do
             {
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("Du måste mata in minst två löner för att kunna göra en beräkning\n");
+                numberOfInputSalaries = ReadInt("Ange antal löner du vill mata in: ");
+
+                if (numberOfInputSalaries < 2)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Du måste mata in minst två löner för att kunna göra en beräkning\n");
+                    Console.ResetColor();
+                }
+
+                else
+                {
+                    ProcessSalaries(numberOfInputSalaries);
+                }
+
                 Console.BackgroundColor = ConsoleColor.Green;
-                Console.WriteLine("Tryck på en tangent för att göra en ny beräkning. Esc avslutar programmet.");
+                Console.WriteLine("Tryck på en tangent för att göra en ny beräkning. Escape avslutar programmet.");
                 Console.ResetColor();
-                //Console.ReadKey(true).Key != ConsoleKey.Escape;
-            } 
-            else 
-            {
-                ProcessSalaries(numberOfInputSalaries);
-            }
-
-            //int salary;
-            //salary = ReadInt("Ange lön nummer {}: ");
-
+                
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
         }
 
         private static void ProcessSalaries(int count)
-        {
+        {                                                                                   
             int[] salaries = new int[count];
 
             for (int i = 0; i < salaries.Length; i++)
             {
                 salaries[i] = ReadInt(string.Format("Ange lön nummer {0}: ", (i + 1)));
-                
-                //Console.Write("Ange lön nummer{0}: ", (i+1));
-                //salaries[i] = int.Parse(Console.ReadLine());
-                //ReadInt(salaries[i]);
-                //salaries[i] = ReadInt("Ange lön nummer{0}: ", (i+1));
             }
+
+       
+            //Kopiera och sortera arrayen
+            int[] copyOfSalaries = (int[])salaries.Clone();
+            Array.Sort(copyOfSalaries);
+
+            Console.WriteLine("----------------------------------------");
+
+            //Beräkna median
+            int medianSalary = copyOfSalaries[count / 2];
+
+            if (count % 2 == 1)
+            {
+                Console.WriteLine("Medianlön: {0,15:c0}", medianSalary);
+            }
+
+            else
+            {
+                medianSalary = (copyOfSalaries[count / 2] + copyOfSalaries[count / 2 - 1]) / 2;
+                Console.WriteLine("Medianlön: {0,15:c0}", medianSalary);
+            }
+
+            //Beräkna medellön
+            double averageSalary;
+            averageSalary = copyOfSalaries.Average();
+            Console.WriteLine("Medellön: {0,16:c0}", averageSalary);
+
+            //Beräkna lönespridningen
+            int wageSpread;
+            wageSpread = copyOfSalaries.Max() - copyOfSalaries.Min();
+            Console.WriteLine("Lönespridning: {0,11:c0}", wageSpread);
+
+            Console.WriteLine("----------------------------------------");
+
+            for (int i = 0; i < salaries.Length; ++i)
+            {
+                Console.Write("{0,10}", salaries[i]);
+
+                if (i % 3 == 2)
+                {
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine();
+
+
         }
 
         private static int ReadInt(string prompt)
         {
             int inputSalaries;
             Console.Write(prompt);
+            string input = null;
 
             while (true)
             {
                 try
                 {
-                    inputSalaries = int.Parse(Console.ReadLine());
+                    input = Console.ReadLine();
+                    inputSalaries = int.Parse(input);
                     return inputSalaries;
                 }
 
@@ -66,9 +111,9 @@ namespace _1dv405_lw222gu_1_3_godtycklig_lon
                 {
                     Console.BackgroundColor = ConsoleColor.Red;
                     Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine("Du måste ange antalet löner med ett heltal.");
+                    Console.WriteLine("Du måste ange ett heltal. {0} kan inte tolkas som ett heltal.", input);
                     Console.ResetColor();
-                    Console.Write("Ange antal löner på nytt: ");
+                    Console.Write("Ange heltalet på nytt: ");
                 }
             }
 
